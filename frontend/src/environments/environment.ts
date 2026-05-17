371d2c7789d193281ff837fa4ee20f2f4484e847
edtarsz <eduardotalaverar1@gmail.com>
2026-05-16 20:57:01 -0700

    feature(Styles) added correct font name and route

--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-
-    supabaseUrl: 'https://gjznuieaxrekydyyoyst.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdqem51aWVheHJla3lkeXlveXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MTA3ODAsImV4cCI6MjA5NDI4Njc4MH0.rYcsr7S2VJPKLVqpWgHYHvHXZlZKIpYHZqLi0yWcpgg'
-}
