15bb9810bb71104966ce5752c6709ad243a85bed
Eduardo Talavera Ramos <eduardotalaverar1@gmail.com>
2026-05-20 19:49:17 -0700

    fix(.env) add new envs

--- frontend/scripts/set-env.js
+++ frontend/scripts/set-env.js
@@ -3,7 +3,11 @@ import { writeFileSync } from 'fs';
 const content = `
 export const environment = {
   production: true,
-  apiUrl: '${process.env.API_URL}'
+  apiUrl: '${process.env.API_URL}',
+  supabaseUrl: '${process.env.SUPABASE_URL}',
+  supabaseKey: '${process.env.SUPABASE_KEY}',
+  tmdbUrl: '${process.env.TMDB_URL}',
+  tmdbKey: '${process.env.TMDB_KEY}'
 };
 `;
 
