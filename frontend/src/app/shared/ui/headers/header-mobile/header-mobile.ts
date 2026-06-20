ad5761cef285adac79d9263be2f15913e5a632ee
Eduardo Talavera <eduardotalaverar1@gmail.com>
2026-06-20 14:05:56 -0700

    fix(Swipe) add border

--- frontend/src/app/shared/ui/headers/header-mobile/header-mobile.ts
+++ frontend/src/app/shared/ui/headers/header-mobile/header-mobile.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideGalleryHorizontalEnd, LucideThumbsUp } from '@lucide/angular';
+import { Separator } from "@shared/ui/separator/separator";
 
 @Component({
     selector: 'app-header-mobile',
-    imports: [LucideGalleryHorizontalEnd, LucideThumbsUp, RouterLink],
+    imports: [LucideGalleryHorizontalEnd, LucideThumbsUp, RouterLink, Separator],
     templateUrl: './header-mobile.html',
     styleUrl: './header-mobile.css',
 })
